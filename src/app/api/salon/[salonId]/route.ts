de4ed247d153a1aa20392e4fbbda4174c1417b25
neolaner <neolanerofficial.com>
2024-12-24 09:26:56 +0330

    add commands

--- src/app/api/salon/[salonId]/route.ts
+++ src/app/api/salon/[salonId]/route.ts
@@ -6,8 +6,10 @@ import { createErrorResponse } from "@/utils/errorHandler";
 import { NextRequest } from "next/server";
 import { z } from "zod";
 
+//extract out the service methods
 const { getSalonById } = salonService();
 
+// define schema for validating params or request.body object
 const paramsSchema = z.object({
   salonId: z.string(),
 });
@@ -20,15 +22,17 @@ export async function GET(
     //1) validation
     const validatedParams = paramsSchema.parse(await params);
 
-    //2) handler
+    //2) handler (just used for getting and sending data to redis and rabbit)
     const data = getSalonById(validatedParams.salonId);
 
     //3) response
-    // if data is null or an empty object
+    // if data is null or an empty object return 404
     if (!data || Object.keys(data).length === 0)
       return Response.json({ error: "salon not found" }, { status: 404 });
+    //if is everything is ok
     return Response.json(data);
   } catch (error) {
+    //error handling
     return createErrorResponse(error);
   }
 }
--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -2,9 +2,11 @@ import { NextResponse } from "next/server";
 import { z } from "zod";
 
 export function createErrorResponse(error: unknown) {
+  //if there is validation error return zod error messages
   if (error instanceof z.ZodError) {
     return NextResponse.json({ error: error.errors }, { status: 400 });
   } else {
+    //otherwise log an error and send generic response to user
     console.error(error);
     return NextResponse.json(
       { error: "Internal Server Error" },
